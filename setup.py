3086aa650f6bdff0cfd8a32e051aaee068a5a13e
redten-ops <info@redten.io>
2023-11-08 21:07:59 +0000

    version bump for new pypi readme

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "examples/get-ai-result.py",
         "examples/review-answer.py",
     ],
-    version="1.0.0",
+    version="1.0.1",
     license="Apache 2.0",
     description=(
         "python client for redten - "
