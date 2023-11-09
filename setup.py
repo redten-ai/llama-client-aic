06221780b643475c3370cfacdcc9b95b9a28abe2
redten-ops <info@redten.io>
2023-11-09 04:58:17 +0000

    better link for the s3 upload support with the rust rest api

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "examples/get-ai-result.py",
         "examples/review-answer.py",
     ],
-    version="1.0.5",
+    version="1.0.6",
     license="Apache 2.0",
     description=(
         "python client for redten - "
